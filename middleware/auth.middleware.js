73658012c5c6875b44c1386f7b0b6ab6ba808b68
Prabhat Pathak <klaus007p@gmail.com>
2026-08-12 21:04:51 +0530

    Added authorization as the middleware

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,7 +1,35 @@
 import jwt from 'jsonwebtoken'
-import { User } from '../models/user.models'
+import { User, User } from '../models/user.models'
 import { asyncHandler } from '../utils/asyncHandler'
 import { ApiError } from '../utils/ApiError'
 
+// If Auuthorization  fails it will throw errors from apiError
 
-export const protect 
+export const protect = asyncHandler(async(req, res, next) => {
+
+    let token;
+    const header = req.headers.authorization;
+
+    if(header && header.startsWith("Bearer")) {
+        token = header.split(" ") [1];
+    }
+
+    if(!token){
+        throw new ApiError(401, "Not Authorized, no token found");
+    }
+
+    let decode;
+    try {
+        decode = jwt.verify(token, process.env.JWT_SECRET)
+    } catch (error) {
+        throw new ApiError(401, "Not Authorized, token invalid or expired")
+    }
+
+    const user = await User.findById(decode.id);
+    if(!user){
+        throw new ApiError (401, "Not Authorized, User no longer exists")
+    }
+
+    req.user = user;
+    next(); // Route Handler
+})
